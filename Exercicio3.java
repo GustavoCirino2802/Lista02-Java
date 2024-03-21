ffaf2bd964b07707d29df7cf105e0918417c848c
Auto Logon <autologon@CSED.LAB>
2024-03-21 09:48:01 -0300

    Exercicio03

---
+++ Exercicio3.java
@@ -0,0 +1,19 @@
+public class Exercicio3 {
+
+    public static void executar(){ 
+
+    int vetor[] = new int [5];
+    int dobro[] = new int [5];
+
+    for (int i = 0; i < 5; i++) {
+
+        vetor[i] = Prompt.lerInteiro("Digite 5 números: ");
+        dobro[i] = (vetor[i] *2);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        Prompt.imprimir("O " + (i + 1) + "º" + " número do vetor é = " + dobro[i]);   
+        Prompt.separador();
+    }
+        } 
+    }
